e672fbac7b8a2f6cadb5c30681f91b64307aaf6c
jpm <jpmckenna90@gmail.com>
2020-01-20 23:06:24 -0500

    Started adding cards for portfolio pieces.

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
   $(window).scroll(function () {
       if ($(document).scrollTop() > 120) {
           $("nav").addClass("scrolled");
+          $(".brand-logo").addClass("scrolled");
       } else {
           $("nav").removeClass("scrolled");
+          $(".brand-logo").removeClass("scrolled");
       }
   });
 
