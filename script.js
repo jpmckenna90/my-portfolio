da991b88d22e8abdbbca56c88396c5ed63490912
jpm <jpmckenna90@gmail.com>
2020-01-21 22:47:30 -0500

    made a bunch of questionable decisions. Implemented scrollspy so I can rip it out later. Started building contact.

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 $(document).ready(function () {
+
   $(window).scroll(function () {
+
       if ($(document).scrollTop() > 120) {
           $("nav").addClass("scrolled");
           $(".brand-logo").addClass("scrolled");
+          $(".navlist").addClass('navstack');
+          $(".navbar-fixed").addClass("valign-wrapper");
       } else {
           $("nav").removeClass("scrolled");
           $(".brand-logo").removeClass("scrolled");
+          $(".navlist").removeClass('navstack');
+          $(".navbar-fixed").removeClass("valign-wrapper");
       }
   });
 
+  
+  
+
+
+  
   $(".sidenav").sidenav();
+  $('.scrollspy').scrollSpy();
+
+
 });
