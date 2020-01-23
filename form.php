dec57192c1875a41ef7d6ff538e1ad60b2ec8897
jpm <jpmckenna90@gmail.com>
2020-01-23 16:57:10 -0500

    Added resume.

--- form.php
+++ form.php
@@ -1,14 +1,23 @@
 <?php
-  $firstname = $_POST['firstname'];
-  $lastname = $_POST['lastname']
-  $visitor_email = $_POST['emailaddress'];
-  $message = $_POST['message'];
+  $firstname = $_POST["firstname"];
+  $lastname = $_POST["lastname"];
+  $visitor_email = $_POST["emailaddress"];
+  $message = $_POST["message"];
+
+  // if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['emailaddress']) && isset($_POST[]))
+
+  // $firstname = 'test';
+  // $lastname = 'test';
+  // $visitor_email = 'test@test.com';
+  // $message = 'test';
+
+
 
   $from = 'jpmckenna90@gmail.com';
   $email_subject = "New Email from Portfolio";
-  $email_body = "New message from $firstname $lastname. \n $message"
+  $email_body = "New message from $firstname $lastname. \n $message";
 
   $to = 'jpmckenna90@gmail.com';
 
-  mail($to,$email_subject,$email_body);
+  mail($to,$from,$email_subject,$email_body);
 ?>
