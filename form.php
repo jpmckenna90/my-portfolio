6776bf15f9f446122bcb6c3a24a6d80513b45102
jpm <jpmckenna90@gmail.com>
2020-01-23 13:24:49 -0500

    Getting ambitious and trying to add some PHP to send emails from form...fingers crossed?

---
+++ form.php
@@ -0,0 +1,14 @@
+<?php
+  $firstname = $_POST['firstname'];
+  $lastname = $_POST['lastname']
+  $visitor_email = $_POST['emailaddress'];
+  $message = $_POST['message'];
+
+  $from = 'jpmckenna90@gmail.com';
+  $email_subject = "New Email from Portfolio";
+  $email_body = "New message from $firstname $lastname. \n $message"
+
+  $to = 'jpmckenna90@gmail.com';
+
+  mail($to,$email_subject,$email_body);
+?>
